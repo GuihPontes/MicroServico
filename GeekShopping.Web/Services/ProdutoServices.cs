a95c9aaa30787ddfa16a2fb059c39faa27b862c7
Guilherme <guilherme-pontes2015@hotmail.com>
2022-10-09 20:00:18 -0300

    Comunicação front e back

---
+++ GeekShopping.Web/Services/ProdutoServices.cs
@@ -0,0 +1,51 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Util;
+namespace GeekShopping.Web.Services
+{
+    public class ProdutoServices : IProdutoServices
+    {
+       private readonly HttpClient _client;
+        public const string BasePath = "api/v1/produto";
+
+        public ProdutoServices(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ProdutoModel> BuscaProduto(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProdutoModel>();
+        }
+
+        public async Task<IEnumerable<ProdutoModel>> BuscaTodos()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProdutoModel>>();
+        }
+
+        public async Task<ProdutoModel> Criar(ProdutoModel produto)
+        {
+            var response = await _client.PostAsJason(BasePath, produto);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProdutoModel>();
+            else throw new Exception("Deu algo errado na comunicação");
+
+        }
+        public async Task<ProdutoModel> Atualizar(ProdutoModel produto)
+        {
+            var response = await _client.PutAsJason(BasePath, produto);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProdutoModel>();
+            else throw new Exception("Deu algo errado na comunicação");
+        }
+        public async Task<bool> Deletar(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else throw new Exception("Deu algo errado na comunicação");
+        }
+    }
+}
---
+++ GeekShopping.Web/Util/HttpClientExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Util
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/jason");
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode) throw new ApplicationException($"Algo aconteceu de errado na comunicação : {response.ReasonPhrase}");
+
+            
+                var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+
+        public static Task<HttpResponseMessage> PostAsJason<T>(this HttpClient response, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return response.PostAsync(url, content);
+        } 
+        public static Task<HttpResponseMessage> PutAsJason<T>(this HttpClient response, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return response.PutAsync(url, content);
+        }
+        
+
+    }
+}
