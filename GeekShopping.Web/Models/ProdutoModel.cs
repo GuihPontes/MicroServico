5dbb24adfad577a43a3e969f6b988e9e856405af
Guilherme <guilherme-pontes2015@hotmail.com>
2022-10-09 19:00:53 -0300

    Mapamento para cosumir API fron end

---
+++ GeekShopping.Web/Models/ProdutoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class ProdutoModel
+    {
+        public long? Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal? Preco { get; set; }
+        public string? Descricao { get; set; }
+        public string? Categoria { get; set; }
+        public string? URL { get; set; }
+    }
+}
---
+++ GeekShopping.Web/Services/IServices/IProdutoServices.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProdutoServices
+    {
+        Task<IEnumerable<ProdutoModel>> BuscaTodos();
+        Task<ProdutoModel> BuscaProduto(long id);
+        Task<ProdutoModel> Criar(ProdutoModel produto);
+        Task<ProdutoModel> Atualizar(ProdutoModel produto);
+        Task<bool> Deletar(long id);
+    }
+}
